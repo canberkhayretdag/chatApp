7d611c65fe304d67ad100f6ae39393ed324dcbfa
canberkhayretdag <info@canberkhayretdag.com>
2018-09-16 14:55:18 +0300

    google+ login and save database

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const passport = require('passport');
 const logger = require('morgan');
 const dotenv = require('dotenv');
 dotenv.config();
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const auth = require('./routes/auth');
 
 const app = express();
 
@@ -23,8 +24,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'bower_components')));
 
+app.use(passport.initialize());
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/auth', auth);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ auth/google.js
@@ -0,0 +1,35 @@
+const passport = require('passport');
+const googleStrategy = require('passport-google-oauth20');
+
+//models
+const User = require('../models/users');
+
+passport.use(new googleStrategy({
+    clientID: process.env.GOOGLE_LOGIN_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_LOGIN_SECRET_ID,
+    callbackURL: process.env.GOOGLE_LOGIN_CALLBACK_URL
+}, ((accessToken, refreshToken, profile, done) => {
+    const data = profile._json;
+    console.log(data);
+
+    User.findOrCreate ({
+        'googleId': data.id
+    },{
+        name:data.name.givenName,
+        surname: data.name.familyName,
+        profilePhotoUrl: data.image.url + '0'
+    }, (err, user) => {
+        return done(err, user);
+    })
+
+})));
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
+
+module.exports = passport;
